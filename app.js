b4046d89871e9f80a7141af9573d2e3ca1d2ce37
Anahid <anahidhemmat2130@gmail.com>
2022-03-09 11:57:11 +0400

    selectors

---
+++ app.js
@@ -0,0 +1,5 @@
+//selectors
+const container = document.querySelector(".container");
+const searchForm = document.querySelector("form");
+const searchInput = document.querySelector("input");
+const searchResultSection = document.querySelector(".serach-result");
