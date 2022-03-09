ff273d6d52e9f6f7c365e06d3226b50952ec6bec
Anahid <anahidhemmat2130@gmail.com>
2022-03-09 12:00:57 +0400

    form submit

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ const container = document.querySelector(".container");
 const searchForm = document.querySelector("form");
 const searchInput = document.querySelector("input");
 const searchResultSection = document.querySelector(".serach-result");
+let searchInputValue;
+
+//form submit
+
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchInputValue = searchInput.value;
+  console.log(searchInputValue);
+  searchForm.reset();
+});
