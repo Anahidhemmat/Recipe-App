37f9a1505dd319a958baa65659d31e0725b55e4e
Anahid <anahidhemmat2130@gmail.com>
2022-03-10 16:43:25 +0400

    selectors

--- RecipeApp2/app.js
+++ RecipeApp2/app.js
@@ -1,3 +1,8 @@
-fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=egg`)
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+//selectors
+const searchForm = document.querySelector("form");
+const searchInput = document.querySelector(".search-input");
+const searchResultContainer = document.querySelector(".results");
+
+// fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=egg`)
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
